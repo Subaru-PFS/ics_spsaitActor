4ab15d80806b0a981f9f9d9982256b06a81fff1d
Arnaud Le Fur <alefur@lam.fr>
2017-06-07 14:03:01 +0000

    wrong place to put background, I will create a CalibCmd instead

--- python/spsaitActor/Commands/ExposeCmd.py
+++ python/spsaitActor/Commands/ExposeCmd.py
@@ -20,10 +20,9 @@ class ExposeCmd(object):
         #
         self.name = "expose"
         self.vocab = [
-            ('expose', 'object <exptime> [<comment>]', self.doExposure),
+            ('expose', '[object] <exptime> [<comment>]', self.doExposure),
             ('expose', 'flat <exptime> [<attenuator>] [switchOff]', self.doFlat),
             ('expose', 'arc <exptime> [@(ne|hgar|xenon)] [<attenuator>] [switchOff]', self.doArc),
-            ('expose', 'background <exptime>', self.doBackground),
         ]
 
         # Define typed command arguments for the above commands.
@@ -189,62 +188,7 @@ class ExposeCmd(object):
         exptime = cmdKeys['exptime'].values[0]
         comment = "comment='%s'" % cmdKeys['comment'].values[0] if "comment" in cmdKeys else ""
 
-        expType = "object"
-
-        try:
-            if exptime <= 0:
-                raise Exception("exptime must be positive")
-
-            [state, mode, position] = enuKeys.keyVarDict['shutters'].getValue()
-            if not (state == "IDLE" and position == "close") or self.stopExposure:
-                raise Exception("shutters not in position")
-
-        except Exception as e:
-            cmd.fail("text='%s'" % formatException(e, sys.exc_info()[2]))
-            return
-
-        [state, mode, position] = enuKeys.keyVarDict['shutters'].getValue()
-        if not (state == "IDLE" and position == "close") or self.stopExposure:
-            raise Exception("aborting exposure")
-
-        try:
-            cmdCall(actor='ccd_r1', cmdStr="wipe", timeLim=60, forUserCmd=cmd)
-
-            state = ccdKeys.keyVarDict['exposureState'].getValue()
-            if state != "integrating" or self.stopExposure:
-                raise Exception("aborting exposure")
-
-            cmdCall(actor='enu', cmdStr="shutters expose exptime=%.3f" % exptime, forUserCmd=cmd)
-            dateobs = enuKeys.keyVarDict['dateobs'].getValue()
-            exptime = enuKeys.keyVarDict['exptime'].getValue()
-
-            if np.isnan(exptime):
-                raise Exception("Exposure did not occur as expected (interlock ?) Aborting ... ")
-
-            cmdCall(actor='ccd_r1',
-                    cmdStr="read %s exptime=%.3f obstime=%s %s" % (expType, exptime, dateobs, comment),
-                    timeLim=300, forUserCmd=cmd)
-
-        except Exception as e:
-            cmdCall(actor='ccd_r1', cmdStr="clearExposure", forUserCmd=cmd)
-            cmd.fail("text='%s'" % formatException(e, sys.exc_info()[2]))
-            return
-
-        cmd.finish("text='exposure done exptime=%.2f'" % exptime)
-
-    @threaded
-    def doBackground(self, cmd):
-        self.resetExposure()
-
-        cmdKeys = cmd.cmd.keywords
-        cmdCall = self.actor.safeCall
-        enuKeys = self.actor.models['enu']
-        ccdKeys = self.actor.models['ccd_r1']
-        dcbKeys = self.actor.models['dcb']
-
-        exptime = cmdKeys['exptime'].values[0]
-
-        expType = "arc"
+        expType = "object" if "object" in cmdKeys else "arc"
 
         try:
             if exptime <= 0:
@@ -280,4 +224,4 @@ class ExposeCmd(object):
             cmd.fail("text='%s'" % formatException(e, sys.exc_info()[2]))
             return
 
-        cmd.finish("text='arc done exptime=%.2f'" % exptime)
+        cmd.finish("text='exposure done exptime=%.2f'" % exptime)
