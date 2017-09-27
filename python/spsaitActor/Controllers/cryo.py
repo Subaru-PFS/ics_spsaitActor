1727257a061778caf06e9c69444449c897d4e324
Arnaud Le Fur <alefur@lam.fr>
2017-09-27 16:56:05 +0000

    leakback procedure okay, regeneration not complete yet, class Threshold to handle cmd if keyword goes above specified Threshold

--- python/spsaitActor/Controllers/cryo.py
+++ python/spsaitActor/Controllers/cryo.py
@@ -1,7 +1,10 @@
 import logging
+import time
 
 from actorcore.QThread import QThread
-from spsaitActor.utils import CmdSeq
+from spsaitActor.utils import CmdSeq, xcuData
+
+
 
 class cryo(QThread):
     def __init__(self, actor, name, loglevel=logging.DEBUG):
@@ -13,25 +16,82 @@ class cryo(QThread):
         QThread.__init__(self, actor, name, timeout=2)
         self.logger = logging.getLogger(self.name)
         self.logger.setLevel(loglevel)
+        self.xcuDatas = {}
+        self.attachCallbacks()
 
-    def leakback(self, xcuActor, duration):
+    @property
+    def roughGauge(self):
+        model = self.actor.models['xcu_r1']
+        keyvar = model['roughGauge1']
+        try:
+            val = keyvar.getValue()
+        except ValueError:
+            val = None
+        return val
 
-        seqClosing = [CmdSeq(xcuActor, "gauge status"),
-                      CmdSeq(xcuActor, "gatevalve close", tempo=5),
-                      CmdSeq(xcuActor, "gatevalve status")]
+    def startPumps(self, xcuActor, ionPumpsOn, turboOn, gvOpen):
 
-        seqCheck = [CmdSeq(xcuActor, "gauge status", tempo=duration * 60),
-                    CmdSeq(xcuActor, "gatevalve status"),
-                    CmdSeq(xcuActor, "gauge status"),
-                    CmdSeq(xcuActor, "turbo status")]
+        sequence = [CmdSeq(xcuActor, "gatevalve open", timeLim=90)] if (turboOn and gvOpen) else []
+        sequence += ([CmdSeq(xcuActor, "ionpump on", timeLim=90)] if ionPumpsOn else [])
+        sequence += self.sample(xcuActor)
 
-        seqOpening = [CmdSeq(xcuActor, "gatevalve open", tempo=5),
-                      CmdSeq(xcuActor, "gatevalve status")]
+        return sequence
 
-        return seqClosing, seqCheck, seqOpening
+    def stopPumps(self, xcuActor, ionPumpsOn, gvOpen):
+        sequence = [CmdSeq(xcuActor, "ionpump off", timeLim=90)] if ionPumpsOn else []
+        sequence += ([CmdSeq(xcuActor, "gatevalve close")] if gvOpen else [])
+        sequence += self.sample(xcuActor)
 
+        return sequence
+
+    def sample(self, xcuActor, cmd=None, keys=None):
+        keys = ["turbo", "gauge", "cooler", "gatevalve", "ionpump"] if keys is None else keys
+        sequence = [CmdSeq(xcuActor, "%s status" % key, doRetry=True, tempo=1.0) for key in keys]
+        if cmd is not None:
+            self.actor.processSequence(self.name, cmd, sequence)
+        else:
+            return sequence
+
+    def startRoughing(self):
+        sequence = [CmdSeq("dcb", "aten switch on channel=roughpump")]
+        return sequence
+
+    def stopRoughing(self):
+        sequence = [CmdSeq("dcb", "aten switch off channel=roughpump")]
+        return sequence
+
+    def startTurbo(self, xcuActor):
+        sequence = [CmdSeq(xcuActor, "turbo start")]
+        return sequence
+
+    def stopTurbo(self, xcuActor):
+        sequence = [CmdSeq(xcuActor, "turbo stop")]
+        return sequence
+
+    def openGV(self, xcuActor):
+        sequence = [CmdSeq(xcuActor, "gatevalve open")]
+        return sequence
+
+    def closeGV(self, xcuActor):
+        sequence = [CmdSeq(xcuActor, "gatevalve close")]
+        return sequence
+
+    def regeneration(self, xcuActor):
+        sequence = [CmdSeq(xcuActor, "ionpump off"),
+                    CmdSeq(xcuActor, "cooler off"),
+                    CmdSeq(xcuActor, "heaters ccd power=100"),
+                    CmdSeq(xcuActor, "heaters spider power=100")]
+        return sequence
+
+    def attachCallbacks(self):
+        for xcu in self.actor.xcus:
+            self.xcuDatas[xcu] = xcuData(self.actor, xcu)
 
     def handleTimeout(self):
         """| Is called when the thread is idle
         """
         pass
+
+    def xcuKeys(self, arm):
+        xcuActor = self.actor.arm2xcu[arm]
+        return xcuActor, self.actor.models[xcuActor]
--- python/spsaitActor/utils.py
+++ python/spsaitActor/utils.py
@@ -1,25 +1,99 @@
+import sys
 import traceback as tb
 from functools import partial
+import time
+from actorcore.QThread import QThread
+
+class Threshold(QThread):
+    def __init__(self, actor, xcuData, name, threshold, vFail, tlim, callback, kwargs):
+        QThread.__init__(self, actor, name, timeout=2)
+        self.xcuData = xcuData
+        self.threshold = threshold
+        self.vFail = vFail
+        self.tlim = tlim
+        self.callback = callback
+        self.kwargs = kwargs
+        self.ret = None
+
+        self.startUp()
+
+    def handleTimeout(self):
+        """| Is called when the thread is idle
+        """
+
+        if self.exitASAP:
+            raise SystemExit()
+
+        if self.vFail is not None and self.xcuData[self.name] >= self.vFail:
+            self.ret = None, None
+            self.exit()
+
+        elif (self.xcuData[self.name] > self.threshold) or time.time() > self.tlim:
+            self.ret = time.time(), self.xcuData[self.name]
+            self.callback(**self.kwargs)
+            self.exit()
+
+    def exit(self):
+        self.exitASAP = True
 
 
-def threaded(func):
-    def wrapper(self, *args, **kwargs):
-        self.actor.controllers[self.name].putMsg(partial(func, self, *args, **kwargs))
+class xcuData(dict):
+    def __init__(self, actor, xcu):
+        dict.__init__(self)
+        self.actor = actor
+        self.xcuKeys = actor.models[xcu]
 
-    return wrapper
+        for key in ['pressure', 'turboSpeed', 'gatevalve', 'ionpump1', 'ionpump2', 'coolerTemps']:
+            keyvar = self.xcuKeys.keyVarDict[key]
+            self[key] = None
+            keyvar.addCallback(self.updateVals)
+
+    def updateVals(self, keyvar):
+        key = keyvar.name
+        try:
+            val = keyvar.getValue()
+        except ValueError:
+            val = None
+
+        self[key] = val
+
+    def addThreshold(self, key, threshold, vFail=None, tlim=None, callback=None, kwargs=None):
+        th = Threshold(self.actor, self, key, threshold, vFail, tlim, callback, kwargs)
+        return th
+
+    @property
+    def turboSpeed(self):
+        return self['turboSpeed']
+
+    @property
+    def pressure(self):
+        return self['pressure']
+
+    @property
+    def gvPosition(self):
+        return self['gatevalve'][1]
+
+    @property
+    def gvControlState(self):
+        return self['gatevalve'][2]
+
+    @property
+    def ionpump1On(self):
+        return self['ionpump1'][0]
+
+    @property
+    def ionpump2On(self):
+        return self['ionpump2'][0]
+
+    @property
+    def tip(self):
+        return self['coolerTemps'][0]
+
+    @property
+    def coolerPower(self):
+        return self['coolerTemps'][3]
 
 
-def formatException(e, traceback):
-    """ Format the caught exception as a string
-
-    :param e: caught exception
-    :param traceback: exception traceback
-    """
-
-    def clean(string):
-        return str(string).replace("'", "").replace('"', "")
-
-    return "%s %s %s" % (clean(type(e)), clean(type(e)(*e.args)), clean(tb.format_tb(traceback, limit=1)[0]))
 
 
 class CmdSeq(object):
@@ -41,43 +115,45 @@ class CmdSeq(object):
                 }
 
 
-def computeRate(start, end, pressure1, pressure2):
-    return 150 * (pressure2 - pressure1) / ((end - start).total_seconds())
-
-
-class CryoException(Exception):
-    def __init__(self, error="Abort cryo requested"):
-        Exception.__init__(self, error)
-
-
-class DetalignException(Exception):
-    def __init__(self, error="Abort detalign requested"):
-        Exception.__init__(self, error)
-
-
-class DitherException(Exception):
-    def __init__(self, error="Abort dither requested"):
-        Exception.__init__(self, error)
-
-
-class ExposeException(Exception):
-    def __init__(self, error="Abort exposure requested"):
-        Exception.__init__(self, error)
-
-
-class CalibException(Exception):
-    def __init__(self, error="Abort calib requested"):
-        Exception.__init__(self, error)
-
-
-class TestException(Exception):
-    def __init__(self, error="Abort test requested"):
-        Exception.__init__(self, error)
-
-
 class FailExposure(list):
     def __init__(self, ccd):
         list.__init__(self)
         self.extend([CmdSeq(ccd, "clearExposure"),
                      CmdSeq(ccd, "disconnect controller=fee", tempo=10),
                      CmdSeq(ccd, "connect controller=fee", tempo=5)])
+
+
+def threaded(func):
+    def tryfunc(self, cmd):
+        try:
+            return func(self, cmd)
+
+        except Exception as e:
+            cmd.fail("text='%s'" % formatException(e, sys.exc_info()[2]))
+            return
+
+    def wrapper(self, cmd):
+        self.actor.controllers[self.name].putMsg(partial(tryfunc, self, cmd))
+
+    return wrapper
+
+
+def formatException(e, traceback):
+    """ Format the caught exception as a string
+
+    :param e: caught exception
+    :param traceback: exception traceback
+    """
+
+    def clean(string):
+        return str(string).replace("'", "").replace('"', "")
+
+    return "%s %s %s" % (clean(type(e)), clean(type(e)(*e.args)), clean(tb.format_tb(traceback, limit=2)[-1]))
+
+
+
+def computeRate(start, end, pressure1, pressure2):
+    return 150 * (pressure2 - pressure1) / (end - start)
+
+
+
